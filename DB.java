6869f0df27eeb77c2fbceb4edc0f8094cfd0b824
galuh ramaditya <galuh.ramaditya@gmail.com>
2019-04-28 23:46:56 +0700

    Add files via upload

---
+++ DB.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author galuhramaditya
+ */
+public class DB {
+    private static final Connection conn = Connecting.DB();
+    public static ResultSet sql;
+    
+    public static void setSql(String sql) throws SQLException {
+        // set ResultSet with sql syntax
+        Statement stm = conn.createStatement();
+        DB.sql = stm.executeQuery(sql);
+    }
+    
+    public static void execute(String sql) throws SQLException {
+        // execute sql syntax
+        PreparedStatement pst = conn.prepareStatement(sql);
+        pst.execute();
+    }
+}
